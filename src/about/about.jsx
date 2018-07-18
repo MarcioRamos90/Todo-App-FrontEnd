f973948a25cda943bd9c478d49371289a3e578b0
marcioalvesramos90@hotmail.com <marcioalvesramos90@hotmail.com>
2018-07-18 16:50:47 -0300

    criando o layout inicial

---
+++ src/about/about.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import PageHeader from "../template/pageHeader";
+
+export default props => (
+  <div>
+    <PageHeader name="Sobre" small="Nós" />
+    <h3>Nossa história</h3>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis
+      omnis nam esse assumenda fugit facilis similique nulla, sit laboriosam
+      earum reprehenderit, dicta accusamus mollitia recusandae ea illo non
+      pariatur consequuntur.
+    </p>
+    <h3>Missão e Visão</h3>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis
+      omnis nam esse assumenda fugit facilis similique nulla, sit laboriosam
+      earum reprehenderit, dicta accusamus mollitia recusandae ea illo non
+      pariatur consequuntur.
+    </p>
+    <h3>Imprensa</h3>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis
+      omnis nam esse assumenda fugit facilis similique nulla, sit laboriosam
+      earum reprehenderit, dicta accusamus mollitia recusandae ea illo non
+      pariatur consequuntur.
+    </p>
+  </div>
+);
